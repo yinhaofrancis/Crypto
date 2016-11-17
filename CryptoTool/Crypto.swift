588f3c4bbfa9a6f86def34cdae5b669469da5374
yinhao <yinhao@cheerapp.cn>
2016-11-17 14:28:57 +0800

    d

---
+++ CryptoTool/Crypto.swift
@@ -0,0 +1,18 @@
+//
+//  Crypto.swift
+//  Crypto
+//
+//  Created by YinHao on 2016/11/17.
+//  Copyright © 2016年 Suzhou Qier Network Technology Co., Ltd. All rights reserved.
+//
+
+import Foundation
+import Security
+public func SHA_256(string:String,count:UInt32)->[UInt8]{
+    var md:[UInt8] = Array(repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
+    CC_SHA256(string,count,&md)
+    return md
+}
+public func SHA(string:String)->[UInt8]{
+    
+}
---
+++ KeyChainSavePassword/KeyChainSavePassword.h
@@ -0,0 +1,19 @@
+//
+//  KeyChainSavePassword.h
+//  KeyChainSavePassword
+//
+//  Created by YinHao on 2016/11/17.
+//  Copyright © 2016年 Suzhou Qier Network Technology Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeyChainSavePassword.
+FOUNDATION_EXPORT double KeyChainSavePasswordVersionNumber;
+
+//! Project version string for KeyChainSavePassword.
+FOUNDATION_EXPORT const unsigned char KeyChainSavePasswordVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeyChainSavePassword/PublicHeader.h>
+
+
